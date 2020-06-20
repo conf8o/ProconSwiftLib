bc764b222ae2a2b6b1e1fc2e56ac4ebe69774b76
conf8o <8confeito8@gmail.com>
2020-06-20 23:10:08 +0900

    add fast combination

--- Source/Combinatorics.swift
+++ Source/Combinatorics.swift
@@ -78,3 +78,49 @@ private struct Permutations<Seq: Sequence>: Sequence, IteratorProtocol {
         return nil
     }
 }
+
+private func pow(_ b: Int, _ n: Int, _ p: Int) -> Int {
+    var (b, n) = (b, n)
+    var x = 1
+    while n > 0 {
+        if n % 2 == 0 {
+            b *= b
+            n /= 2
+        } else {
+            x *= b
+            n -= 1
+        }
+    }
+    return x
+}
+
+private struct FastCombination {
+    var mod: Int
+    var fac: [Int]
+    var finv: [Int]
+    var inv: [Int]
+    
+    init(max: Int, mod: Int) {
+        self.mod = mod
+        self.fac = [Int](repeating: 0, count: max)
+        self.finv = [Int](repeating: 0, count: max)
+        self.inv = [Int](repeating: 0, count: max)
+        
+        fac[0] = 1
+        fac[1] = 1
+        finv[0] = 1
+        finv[1] = 1
+        inv[1] = 1
+        
+        for i in 2..<max {
+            fac[i] = fac[i-1] * i % mod
+            inv[i] = mod - inv[mod%i] * (mod / i) % mod
+            finv[i] = finv[i-1] * inv[i] % mod
+        }
+    }
+    
+    func C(_ n: Int, _ r: Int) -> Int {
+        guard n >= r || n < 0 || r < 0 else { return 0 }
+        return fac[n] * (finv[r] * finv[n-r] % mod) % mod
+    }
+}
