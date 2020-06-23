18d823bd6e5aa5fe9c6df5bb5e5f919a0f1066a8
conf8o <41929738+conf8o@users.noreply.github.com>
2020-06-23 12:43:17 +0900

    delete pow

--- Source/Combinatorics.swift
+++ Source/Combinatorics.swift
@@ -79,21 +79,6 @@ private struct Permutations<Seq: Sequence>: Sequence, IteratorProtocol {
     }
 }
 
-private func pow(_ b: Int, _ n: Int, _ p: Int) -> Int {
-    var (b, n) = (b, n)
-    var x = 1
-    while n > 0 {
-        if n % 2 == 0 {
-            b *= b
-            n /= 2
-        } else {
-            x *= b
-            n -= 1
-        }
-    }
-    return x
-}
-
 private struct FastCombination {
     var mod: Int
     var fac: [Int]
